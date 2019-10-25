a514d6e894844373ac65bf2ab58b1d5c2cccb9a8
Timur BUJALO <tbujalo@e1r8p15.unit.ua>
2019-10-25 19:53:57 +0300

    add registration with writing in database

--- app/controllers/AccountController.class.php
+++ app/controllers/AccountController.class.php
@@ -13,9 +13,9 @@ class AccountController extends Controller{
             if ($this->validate_form($login, $pass)){
                 if ($this->isRegisterUser($login, $pass)){
                     $_SESSION['user_in'] = true;
-                    $_SESSION['user_id'] = $this->model->db->column("SELECT `id` FROM `users` WHERE `login` = :logi AND `password` = :pass",
-                    array('logi' => $login, 'pass' => $pass));
+                    $_SESSION['user_id'] = $this->model->db->column("SELECT `id` FROM `users` WHERE `login` = :logi", array('logi' => $login));
                     header("Location: http://localhost:8080/camagru/account/makePhoto");
+                    exit;
                 }
                 else
                     $param = array('message' => 'Invalid login/password');
@@ -27,16 +27,65 @@ class AccountController extends Controller{
         $this->view->render("Login", $param);
     }
 
+    private function checkUserInput($vars){
+        $errors = [];
+
+        foreach ($vars as $k => $v){
+            if (strlen($v) < 1)
+                $errors[] = "Empty field ".$k;
+        }
+        return ($errors);
+    }
+
     public function registerAction($param = []){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $vars = array ('first_name' => htmlentities($_POST['first_name']),
+                'last_name' => htmlentities($_POST['last_name']),
+                'email' => htmlentities($_POST['email']),
+                'login' => htmlentities($_POST['login']),
+                'password' => htmlentities($_POST['pass']),
+                'confirm' => htmlentities($_POST['confirm']));
+            $errors = $this->checkUserInput($vars);
+            if (!filter_var($vars['email'], FILTER_VALIDATE_EMAIL))
+                $errors[] = "Wrong email format";
+            if (strcmp($vars['password'], $vars['confirm']))
+                $errors[] = "Passwords don't match";
+            unset($vars['confirm']);
+            if (!isset($errors[0])){
+                if ($this->isNewLogin($vars['login'])){
+                    $res = $this->addNewUser($vars);
+                    $_SESSION['user_in'] = true;
+                    $_SESSION['user_id'] = $this->model->db->column("SELECT `id` FROM `users` WHERE `login` = :logi", array('logi' => $vars['login']));
+                    header("Location: http://localhost:8080/camagru/account/makePhoto");
+                    exit;
+                }
+                else{
+                    $errors[] = "This login exists, please enter another";
+                }
+            }
+            $param =  array('errors_message' => $errors);
+        }
         $this->view->render("register", $param);
     }
-    
-    private function isRegisterUser($login, $pass){
-        $res = $this->model->db->row("SELECT * FROM `users` WHERE `login` = :logi AND `password` = :pass",
-                array('logi' => $login, 'pass' => $pass));
-        if (empty($res[0]))
+    private function isNewLogin($login){
+        $res = $this->model->db->column("SELECT `id` FROM `users` WHERE `login` = :logi", array('logi' => $login));
+        if ($res)
             return false;
         return true;
+    }
+
+    private function addNewUser($vars){
+        $res = $this->model->db->column('INSERT INTO `users` (`first_name`, `last_name`, `email`, `login`, `password`)
+        VALUES (:first_name, :last_name, :email, :login, :password);', $vars);
+        return $res;
+    }
+
+    private function isRegisterUser($login, $pass){
+        $res = $this->model->db->column("SELECT `id` FROM `users` WHERE `login` = :logi AND `password` = :pass",
+                array('logi' => $login, 'pass' => $pass));
+        if ($res)
+            return true;
+        return false;
     }
 
     private function validate_form($login, $pass) {
@@ -46,6 +95,7 @@ class AccountController extends Controller{
             return true;
     }
     public function makePhotoAction($param = []){
-        $this->view->render("register", $param);
+        
+        $this->view->render("MakePhoto", $param);
     }
 }
--- app/views/account/register.php
+++ app/views/account/register.php
@@ -1,13 +1,20 @@
 
     <h2>Welcom in registration</h2>
-    <h3>Input your <u> e-maill, login </u> and <u>password </u></h3>
-    <form>
+    <h3>Input your <u> first name, lase name, e-maill, login </u> and <u>password </u></h3>
+    <?php if (isset($errors_message)) foreach ($errors_message as $message) echo '<p style="color:red;">'.$message.'</p>'; ?>
+    <form action="" method="POST">
+        <p>First name</p>
+        <p><input type="text" name="first_name"></p>
+        <p>Last name</p>
+        <p><input type="text" name="last_name"></p>
         <p>E-maill</p>
-        <input type="email" id="email" pattern=".+@globex.com" size="30" required>
+        <input type="email" name="email" size="50" required>
         <p>Login</p>
-        <p><input type="text"></p>
+        <p><input type="text" name="login"></p> 
         <p>Password</p>
-        <p><input type="password"></p>
+        <p><input type="password" name="pass"></p>
+        <p>Confirm password</p>
+        <p><input type="password" name="confirm"></p>
         <p><button>Register</button></p>
     </form>
 </body>
