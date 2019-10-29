597a70fedd559f87fcf43e286ae6a0662771a14d
Timur BUJALO <tbujalo@e1r9p10.unit.ua>
2019-10-29 18:23:35 +0200

    now on screen saved a 5 last photo

--- app/views/account/makePhoto.php
+++ app/views/account/makePhoto.php
@@ -4,12 +4,13 @@ if (!isset($_SESSION['user_in']))
 ?>
 <h2>Make your photo here</h2>
 <video id="video" width="640" height="480" autoplay></video>
-<button id="snap">Make Phto</button>
 <canvas id="canvas" width="640" height="480"></canvas>
-
+<br>
+<button id="snap">Make Phto</button>
+<button id="i" onclick="printI();">print i</button>
 <script>
 var video = document.getElementById('video');
-
+var i = 0;
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         video.srcObject = stream;
@@ -20,8 +21,15 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var video = document.getElementById('video');
 
+function printI(){
+    alert(i);
+}
+
 document.getElementById("snap").addEventListener("click", function() {
-	context.drawImage(video, 0, 0, 640, 480);
+    context.drawImage(video, 0, i * 97, 128, 96);
+    i++;
+    if (i > 4)
+        i = 0;
 });
 }
 </script>
